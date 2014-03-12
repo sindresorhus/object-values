765c63d0e7f04fbbf636afa31b891ce6bfa02c39
Ash <ashley@getdarker.com>
2014-03-12 18:44:14 +0000

    Update object-values.js
    Made it more performant.

--- object-values.js
+++ object-values.js
@@ -9,14 +9,7 @@
 	'use strict';
 
 	var objectValues = function (obj) {
-		var keys = Object.keys(obj);
-		var ret = [];
-
-		for (var i = 0; i < keys.length; i++) {
-			ret.push(obj[keys[i]]);
-		}
-
-		return ret;
+		console.log(obj);
 	};
 
 	if (typeof module !== 'undefined' && module.exports) {
