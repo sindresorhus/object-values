b104b408139d11d13c1def7bf85ca7ff2c7965fa
Federico Brigante <bfred-it@users.noreply.github.com>
2016-12-17 19:30:42 +0900

    ES2015ify and require Node.js 4 (#7)

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 'use strict';
-module.exports = function (obj) {
-	var keys = Object.keys(obj);
-	var ret = [];
-
-	for (var i = 0; i < keys.length; i++) {
-		ret.push(obj[keys[i]]);
-	}
-
-	return ret;
-};
+module.exports = obj => Object.keys(obj).map(i => obj[i]);
